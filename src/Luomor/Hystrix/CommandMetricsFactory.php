0b919cf3971ee225bf373cf1aad346bd0adc05aa
zhangchunsheng <zhangchunsheng423@gmail.com>
2018-06-14 17:24:01 +0800

    2.新功能:add command metrics

--- src/Luomor/Hystrix/CommandMetricsFactory.php
+++ src/Luomor/Hystrix/CommandMetricsFactory.php
@@ -9,6 +9,49 @@ namespace Luomor\Hystrix;
 
 use Luomor\Config\Config;
 
+/**
+ * Class CommandMetricsFactory
+ * Factory to keep track of and instantiate new command metrics objects when needed
+ * @package Luomor\Hystrix
+ */
 class CommandMetricsFactory {
+    /**
+     * @var array
+     */
+    protected $commandMetricsByCommand = array();
 
+    /**
+     * @var StateStorageInterface
+     */
+    protected $stateStorage;
+
+    /**
+     * CommandMetricsFactory constructor.
+     *
+     * @param StateStorageInterface $stateStorage
+     */
+    public function __construct(StateStorageInterface $stateStorage) {
+        $this->stateStorage = $stateStorage;
+    }
+
+    /**
+     * Get command metrics instance by command key for given command config
+     *
+     * @param string $commandKey
+     * @param Config $commandConfig
+     * @return mixed
+     */
+    public function get($commandKey, Config $commandConfig) {
+        if(!isset($this->commandMetricsByCommand[$commandKey])) {
+            $metricsConfig = $commandConfig->get('metrics');
+            $statisticalWindow = $metricsConfig->get('rollingStatisticalWindowInMilliseconds');
+            $windowBuckets = $metricsConfig->get('rollingStatisticalWindowBuckets');
+            $snapshotInterval = $metricsConfig->get('healthSnapshotIntervalInMilliseconds');
+
+            $counter = new MetricsCounter($commandKey, $this->stateStorage, $statisticalWindow, $windowBuckets);
+            $this->commandMetricsByCommand[$commandKey] = new CommandMetrics($counter, $snapshotInterval);
+        }
+
+        return $this->commandMetricsByCommand[$commandKey];
+    }
 }
