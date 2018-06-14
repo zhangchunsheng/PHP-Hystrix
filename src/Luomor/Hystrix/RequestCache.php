95702e12c13fbd48ee89a99088c5f3a7109a6bf4
zhangchunsheng <zhangchunsheng423@gmail.com>
2018-06-14 20:48:08 +0800

    3.新功能:add request cache and log

--- src/Luomor/Hystrix/RequestCache.php
+++ src/Luomor/Hystrix/RequestCache.php
@@ -14,5 +14,70 @@ namespace Luomor\Hystrix;
  * @package Luomor\Hystrix
  */
 class RequestCache {
+    /**
+     * Associative array of results per command key per cache key
+     * @var array
+     */
+    protected $cachedResults = array();
 
+    /**
+     * Clears the cache for a given commandKey only
+     *
+     * @param string $commandKey
+     */
+    public function clearAll($commandKey) {
+        if(isset($this->cachedResults[$commandKey])) {
+            unset($this->cachedResults[$commandKey]);
+        }
+    }
+
+    /**
+     * Clears the cache for a given cacheKey, for a given commandKey
+     *
+     * @param string $commandKey
+     * @param string $cacheKey
+     */
+    public function clear($commandKey, $cacheKey) {
+        if($this->exists($commandKey, $cacheKey)) {
+            unset($this->cachedResults[$commandKey][$cacheKey]);
+        }
+    }
+
+    /**
+     * Attempts to obtain cached result for a given command type
+     *
+     * @param string $commandKey
+     * @param string $cacheKey
+     * @return mixed|null
+     */
+    public function get($commandKey, $cacheKey) {
+        if($this->exists($commandKey, $cacheKey)) {
+            return $this->cachedResults[$commandKey][$cacheKey];
+        }
+
+        return null;
+    }
+
+    /**
+     * Puts request result into cache for a given command type
+     *
+     * @param string $commandKey
+     * @param string $cacheKey
+     * @param mixed $result
+     */
+    public function put($commandKey, $cacheKey, $result) {
+        $this->cachedResults[$commandKey][$cacheKey] = $result;
+    }
+
+    /**
+     * Returns true, if specified cache key exists
+     *
+     * @param string $commandKey
+     * @param string $cacheKey
+     * @return bool
+     */
+    public function exists($commandKey, $cacheKey) {
+        return array_key_exists($commandKey, $this->cachedResults)
+            && array_key_exists($cacheKey, $this->cachedResults[$commandKey]);
+    }
 }
